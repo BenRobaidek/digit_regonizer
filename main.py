3af1ee642efe9c8685dd756583c4220c8768bdfd
Ben Robaidek <brobaidek@gmail.com>
2018-07-17 18:10:00 -0700

    fix size mismatch

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ def main():
                                           batch_size=8,
                                           shuffle=False)
 
-    model = Net(batch_size=8)
+    model = ConvNet()
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adamax(model.parameters())
 
@@ -96,6 +96,28 @@ class Net(nn.Module):
         y = y.reshape(y.size(0), -1)
         print('shape(y)', np.shape(y))
         return self.lin(y)
+
+class ConvNet(nn.Module):
+    def __init__(self, num_classes=10):
+        super(ConvNet, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(1, 16, kernel_size=5, stride=1, padding=2),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(16, 32, kernel_size=5, stride=1, padding=2),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.fc = nn.Linear(7*7*32, num_classes)
+
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = out.reshape(out.size(0), -1)
+        out = self.fc(out)
+        return out
 
 if __name__ == '__main__':
     main()
