32dbab8c2906b3f0a931d81e706037dfc6a1dfa7
Ben Robaidek <brobaidek@gmail.com>
2018-07-17 17:31:16 -0700

    skip moving inp out to gpu

--- main.py
+++ main.py
@@ -60,8 +60,8 @@ def main():
     num_epochs = 2
     for epoch in range(num_epochs):
         for i, (images, labels) in enumerate(train_loader):
-            images = images.to(device)
-            labels = labels.to(device)
+            #images = images.to(device)
+            #labels = labels.to(device)
 
             # Forward pass
             outputs = model(images)
