f86b64c9f5a06b9de925558b3cef9afc24f6602a
Ben Robaidek <brobaidek@gmail.com>
2018-07-17 17:24:19 -0700

    move model to main

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ import sys
 sys.path.append('./data/')
 import model
 import torch
+from torch import autograd, nn
+import torch.nn.functional as F
 import torchvision
 import torchvision.transforms as transforms
 
@@ -50,7 +52,7 @@ def main():
                                           batch_size=8,
                                           shuffle=False)
 
-    m = model.Net()
+    m = Net()
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adamax(model.parameters())
 
@@ -74,7 +76,22 @@ def main():
                 print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'
                        .format(epoch+1, num_epochs, i+1, total_step, loss.item()))
 
+class Net(nn.module):
+    def __init__(self):
+        self.conv1 = nn.Conv2d(in_channels=1,out_channels=4, kernel_size=5, padding=2)
+        self.relu1 = nn.LeakyReLU(negative_slope=0.01, inplace=False)
+        self.conv2 = nn.Conv2d(in_channels=4,out_channels=8, kernel_size=5, padding=2)
+        self.relu2 = nn.LeakyReLU(negative_slope=0.01, inplace=False)
+        self.pool = nn.MaxPool2d(kernel_size=2)
+        self.lin = nn.Linear(in_features=28*28*8, out_features=10)
 
+    def forward(self, inp):
+        y = self.conv1(inp)
+        y = self.relu1(y)
+        y = self.conv2(y)
+        y = self.relu2(y)
+        y = self.pool(y)
+        return self.lin(y)
 
 if __name__ == '__main__':
     main()
